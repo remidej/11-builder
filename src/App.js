ded3fb490447f9fea22d41abb1665ed7f3244ced
Rémi de Juvigny <remidejuvigny@gmail.com>
2017-12-07 21:56:30 +0100

    added ui elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+// Team of the week: https://www.easports.com/fifa/ultimate-team/api/fut/item?jsonParamObject=%7B%22page%22:1,%22quality%22:%22totw_gold%22,%22position%22:%22GK%22%7D
+
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
@@ -8,10 +9,22 @@ class App extends Component {
       <div className="App">
         <div className="Settings">
           <h2 className="Sub-title">Create your lineup!</h2>
+          <input className="Search-player" type="search" placeholder="Search for a player..."/>
+          <Results/>
         </div>
         <div className="Pitch"></div>
       </div>
-    );
+    )
+  }
+}
+
+class Results extends Component {
+  render() {
+    return (
+      <div className="Results">
+        <p>This is a result</p>
+      </div>
+    )
   }
 }
 
