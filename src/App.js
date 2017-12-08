8fef4dd677471b9a5773c820c1c33af7302408d5
Rémi de Juvigny <remidejuvigny@gmail.com>
2017-12-08 01:13:49 +0100

    search results are stored in props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@
 import React, { Component } from 'react'
 import './App.css'
 
+// Prepare API calls
+const proxy = 'https://cors-anywhere.herokuapp.com/'
+const urlStart = 'https://www.easports.com/uk/fifa/ultimate-team/api/fut/item?jsonParamObject=%7B"name":"'
+const urlEnd = '"%7D'
+const request = new XMLHttpRequest();
+request.overrideMimeType("application/json")
+let requestResult
+let requestResults = []
+
 class App extends Component {
   render() {
     return (
@@ -21,16 +30,21 @@ class App extends Component {
 class SearchPlayer extends Component {
   constructor(props) {
     super(props)
-    this.state = { value: '' }
+    this.state = {
+      value: '',
+      results: []
+    }
     // Force method binding to React component
     this.updateSearch = this.updateSearch.bind(this);
   }
 
   updateSearch(event) {
-    this.setState({value: event.target.value})
+    this.setState({ value: event.target.value })
     // TODO: find better fix
     window.setTimeout(() => {
-      populateResults(this.state.value)
+      this.setState({
+        results: getPlayersData(this.state.value)
+      })
     }, 10)
   }
 
@@ -65,8 +79,33 @@ class Pitch extends Component {
   }
 }
 
-const populateResults = (searchValue) => {
-  console.log(searchValue)
+const getPlayersData = (searchValue) => {
+  request.open("GET", `${proxy}${urlStart}${searchValue}${urlEnd}`, true)
+  request.responseType = "json"
+  // Abort previous requests
+  request.abort()
+
+  request.addEventListener("readystatechange", e => {
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        requestResult = request.response
+        requestResults = [] // Reset array
+        for (let i=0; i<requestResult.items.length; i++) {
+          // TODO: Filter out duplicates using baseId property
+          requestResults[i] = {
+            name: requestResult.items[i].lastName,
+            photo: requestResult.items[i].headshotImgUrl,
+            flag: requestResult.items[i].nation.imageUrls.small,
+            club: requestResult.items[i].club.imageUrls.normal.small
+          }
+        }
+      }
+    }
+  })
+
+  request.send()
+  console.log(requestResults)
+  return requestResults
 }
 
 export default App
