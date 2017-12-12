3d768e1022942f4611b745bb9638280863ad9c17
Rémi de Juvigny <remidejuvigny@gmail.com>
2017-12-12 23:08:28 +0100

    better organised crawling output

--- src/scraper.js
+++ src/scraper.js
@@ -5,26 +5,18 @@ const request = require('request')
 const urlToScrape = "https://www.fifaindex.com/fr/players/1/"
 
 const frame = {
-  age: "td[data-title=Age]",
-  lastName: "td[data-title=Nom] a"
+  'players': {
+    '_s': 'tr',
+    '_d': [{
+      'name': 'td[data-title=Nom] a'
+    }]
+  }
 }
-
-let data = {}
 
 request(urlToScrape, (error, response, html) => {
   if (!error && response.statusCode == 200) {
     let $ = cheerio.load(html)
     jsonframe($)
-    $(".table.tabled-striped.players tr").each(() => {
-      //data = $('body').scrape(frame, { string: true })
-      console.log(this.scrape(frame, { string: true }))
-    })
+    console.log( $(".table.table-striped.players tbody").scrape(frame, { string: true }) )
   }
 })
-
-console.log(data)
-
-
-//const result = $('.table.tabled-striped.players tbody').scrape(frame)
-
-//console.log(result)
