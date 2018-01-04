e469fa751b1ecff536e540c94c1bb96609172ecd
Rémi de Juvigny <remidejuvigny@gmail.com>
2018-01-04 18:16:12 +0100

    new log file

--- src/scraper.js
+++ src/scraper.js
@@ -5,7 +5,7 @@ const requestPromise = require('request-promise')
 const fs = require('file-system')
 const removeAccents = require('remove-accents')
 const download = require('image-downloader')
-//const RateLimiter = require('limiter')
+const log = require('log-to-file')
 
 let dataList = []
 let failedDownloads = []
@@ -16,6 +16,7 @@ const urlToScrape = "https://www.fifaindex.com/fr/players/"
 const urlRoot = "https://www.fifaindex.com"
 
 const getData = (url) => {
+	log(`scraping pages : ${Math.trunc(i*100/totalPages)}%`)
 	console.log(`scraping pages : ${Math.trunc(i*100/totalPages)}%`)
 	requestPromise(url)
 		.then((html) => {
@@ -42,6 +43,7 @@ const getData = (url) => {
 			i++
 			// Stop at last page
 			if (i== totalPages) {
+				log('stopped before loading page ' + i)
 				console.log('stopped before loading page ' + i)
 				// Scraping is done, save data to JSON
 				saveImages()
@@ -50,12 +52,14 @@ const getData = (url) => {
 			}
 		})
 		.catch((error) => {
+			log('Crawling failed')
 			console.log('Crawling failed')
 		})
 }
 
 const retryDownloads = () => {
 	for (const fail of failedDownloads) {
+		log(failedDownloads.length)
 		console.log(failedDownloads.length)
 		download
 		.image({
@@ -65,12 +69,14 @@ const retryDownloads = () => {
 			// Delete element from fails list
 			let fixedIndex = failedDownloads.indexOf(fail)
 			failedDownloads.splice(fixedIndex, 1)
+			log('Fixed failed download of ' + fail.url)
 			console.log('Fixed failed download of ' + fail.url)
 			// Save JSON files
 			if (fixedIndex == failedDownloads.length - 1) {
 				savePlayersData()
 			}
 		}).catch(error => {
+			log("Failed again downloading " + fail.url)
 			console.log("Failed again downloading " + fail.url)
 			retryDownloads()
 		})
@@ -90,21 +96,12 @@ const downloadClubLogos = (playerObject) => {
 		})
 		.catch(
 			error => {
-				console.log(
-					"Failed loading " +
-						playerObject
-							.club
-							.logo
-				)
-				failedDownloads.push(
-					{
-						url:
-							playerObject
-								.club
-								.logo,
+				log("Failed loading " + playerObject.club.logo)
+				console.log("Failed loading " + playerObject.club.logo)
+				failedDownloads.push({
+						url: playerObject.club.logo,
 						dest: `public/data/images/clubs/${formattedClubName}.png`
-					}
-				)
+				})
 				downloadFlags(playerObject)
 			}
 		)
@@ -126,6 +123,7 @@ const downloadFlags = (playerObject) => {
 			checkDownloadSuccess(playerObject)
 		})
 		.catch(error => {
+			log("Failed loading " + playerObject.flag)
 			console.log("Failed loading " + playerObject.flag)
 			failedDownloads.push({
 				url: playerObject.flag,
@@ -137,16 +135,19 @@ const downloadFlags = (playerObject) => {
 
 // Check donwload fails
 const checkDownloadSuccess = () => {
+	log(`${Math.trunc(count*10000/dataList.length)/100}%, ${count} out of ${dataList.length}`)
 	console.log(`${Math.trunc(count*100/dataList.length)/100}%, ${count} out of ${dataList.length}`)
   //console.log("getting " + playerObject.name)
   //console.log(`downloading images: ${Math.trunc(count*100/dataList.length)}%`)
   count++
   if (count == dataList.length) {
+    log(`Finished dl with ${failedDownloads.length} fails`)
     console.log(`Finished dl with ${failedDownloads.length} fails`)
     if (failedDownloads.length == 0) {
       // Last loop ended, write json files
       savePlayersData()
     } else {
+      log("download fails: " + failedDownloads)
       console.log("download fails:")
       console.log(failedDownloads)
       retryDownloads()
@@ -158,10 +159,13 @@ const saveImages = () => {
   fs.mkdir('public/data/images/photos')
   fs.mkdir('public/data/images/clubs')
   fs.mkdir('public/data/images/flags')
+	log('datalist length:')
 	console.log('datalist length:')
+  log(dataList.length)
   console.log(dataList.length)
   for (const playerObject of dataList) {
-		console.log('not done')
+		log('not done: ' + playerObject.name)
+		console.log('not done: ' + playerObject.name)
 		// Download player photo
 		download
 			.image({
@@ -169,6 +173,7 @@ const saveImages = () => {
 				dest: "public/data/images/photos/"
 			})
 			.catch((error) => {
+				log('Failed loading ' + playerObject.photo)
 				console.log('Failed loading ' + playerObject.photo)
 				failedDownloads.push({
 					url: playerObject.photo,
@@ -202,7 +207,9 @@ const savePlayersData = () => {
     if (formattedName !== 'undefined') {
       fs.writeFile(`public/data/players/${formattedName}.json`, JSON.stringify(playerObject))
     }
-  }
+	}
+	log(`done with ${failedDownloads.length} fails`)
+	console.log(`done with ${failedDownloads.length} fails`)
 }
 
 getData(`${urlToScrape}${i}/`)
