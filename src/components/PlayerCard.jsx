eb1c9851638e37d8b920f07bfb8d3998c17f6674
Rémi de Juvigny <remidejuvigny@gmail.com>
2018-01-21 12:31:17 +0100

    better file separation

---
+++ src/components/PlayerCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class playerCard extends React.Component {
+  render() {
+    return (
+      <div className="playerCard">
+        <p></p>
+      </div>
+    )
+  }
+}
