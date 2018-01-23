0d721cefb0ce4f7d60aea925244fbd353d1eef63
Rémi de Juvigny <remidejuvigny@gmail.com>
2018-01-23 11:47:48 +0100

    drag and drog players around pitch works

--- src/components/PlayersCards.jsx
+++ src/components/PlayersCards.jsx
@@ -2,14 +2,55 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 
 export default class PlayersCards extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isDragging: false,
+      differenceX: 0,
+      differenceY: 0,
+      originX: 0,
+      originY: 0
+    }
+  }
+
   componentDidMount() {
     ReactDOM.findDOMNode(this).addEventListener('mousedown', e => {
       this.dragStart(e.clientX, e.clientY)
     })
+    ReactDOM.findDOMNode(this).addEventListener('mousemove', e => {
+      this.dragMove(e.clientX, e.clientY)
+    })
+    ReactDOM.findDOMNode(this).addEventListener('mouseup', e => {
+      this.dragEnd(e.clientX, e.clientY)
+    })
+    this.setState({
+      originX: ReactDOM.findDOMNode(this).getBoundingClientRect().x,
+      originY: ReactDOM.findDOMNode(this).getBoundingClientRect().y,
+    })
   }
 
   dragStart = (x, y) => {
-    console.log(`x: ${x}, y: ${y}`)
+    this.setState({ isDragging: true })
+  }
+
+  dragMove = (x, y) => {
+    if (this.state.isDragging) {
+      this.setState({
+        differenceX:  x - this.state.originX,
+        differenceY:  y - this.state.originY
+      })
+      ReactDOM.findDOMNode(this).querySelector('div').style.transform = `
+        translateX(${this.state.differenceX}px)
+        translateY(${this.state.differenceY}px)
+      `
+      ReactDOM.findDOMNode(this).querySelector('div').style.background = 'magenta'
+    }
+
+  }
+
+  dragEnd = (x, y) => {
+    this.setState({ isDragging: false })
+    ReactDOM.findDOMNode(this).querySelector('div').style.background = 'red'
   }
 
   render() {
