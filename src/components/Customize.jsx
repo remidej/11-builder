c32f5ec26c3118e7a9b84f3482f81a72ec955d13
Rémi de Juvigny <remidejuvigny@gmail.com>
2018-02-02 19:48:28 +0100

    :construction: add pitch color menu

--- src/components/Customize.jsx
+++ src/components/Customize.jsx
@@ -4,6 +4,10 @@ import React from 'react'
 const html2canvas = require("html2canvas")
 
 export default class Customize extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { pitchColor: 'green' }
+  }
   
   toggleTacticMenu = () => {
     const tacticButton = document.querySelector('.Tactic')
@@ -11,15 +15,30 @@ export default class Customize extends React.Component {
     if (tacticButton.classList.contains('expanded')) {
       // Collapse menu
       tacticButton.classList.remove('expanded')
-      window.setTimeout(() => {
-        tacticButton.style.zIndex = '100'
-      }, 500)
     } else {
       // Expand menu
-      tacticButton.style.zIndex = '200'
       tacticButton.classList.add('expanded')
-      
     }
+  }
+  
+  toggleColorMenu = () => {
+    const colorButton = document.querySelector('.Pitch-style')
+
+    if (colorButton.classList.contains('expanded')) {
+      // Collapse menu
+      colorButton.classList.remove('expanded')
+    } else {
+      // Expand menu
+      colorButton.classList.add('expanded')
+    }
+  }
+
+  setColor = color => {
+    // Apply color change
+    const pitch = document.querySelector('.Pitch')
+    pitch.style.background = color
+    // Save color change
+    this.setState({ pitchColor: color })
   }
 
   render() {
@@ -27,17 +46,28 @@ export default class Customize extends React.Component {
       <div className="Customize">
         <div
           className="Tactic Menu"
-          onClick={ () => { this.toggleTacticMenu() } }
+          onClick={ () => {this.toggleTacticMenu()} }
         >
           <div className="Options">
-            <div data-tactic="4-3-3">4-3-3</div>
-            <div data-tactic="4-3-3">4-3-3</div>
-            <div data-tactic="4-3-3">4-3-3</div>
-            <div data-tactic="4-3-3">4-3-3</div>
+            <div data-tactic="4-3-3" onClick={() => { this.props.setActiveTactic('4-3-3') }}>4-3-3</div>
+            <div data-tactic="4-3-3" onClick={() => { this.props.setActiveTactic('4-4-2') }}>4-4-2</div>
+            <div data-tactic="4-3-3" onClick={() => { this.props.setActiveTactic('3-5-2') }}>3-5-2</div>
+            <div data-tactic="4-3-3" onClick={() => { this.props.setActiveTactic('3-4-3') }}>3-4-3</div>
           </div>
           <p className="Selected">{`Tactic: ${this.props.activeTactic}`}</p>
         </div>
-        <div className="Pitch-style Menu">Color: green</div>
+        <div
+          className="Pitch-style Menu"
+          onClick={() => {this.toggleColorMenu()} }
+        >
+          <div className="Options">
+            <div data-tactic="4-3-3" onClick={() => { this.setColor('blue') }}>Blue</div>
+            <div data-tactic="4-3-3" onClick={() => { this.setColor('red') }}>Red</div>
+            <div data-tactic="4-3-3" onClick={() => { this.setColor('orange') }}>Orange</div>
+            <div data-tactic="4-3-3" onClick={() => { this.setColor('black') }}>Black</div>
+          </div>
+          <p className="Selected">{`Colour: ${this.state.pitchColor}`}</p>
+        </div>
         <div
           href="#"
           title="Generate lineup"
